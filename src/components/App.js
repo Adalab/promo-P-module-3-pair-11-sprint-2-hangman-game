4417438f93383d89def10b96be57f814b4173767
celiarodriguezmo <celiro02@gmail.com>
2022-04-27 20:08:02 +0200

    add functionality paint dummy with number of errors

--- src/components/App.js
+++ src/components/App.js
@@ -45,6 +45,7 @@ function App() {
     const errorLetters = userLetters.filter(
       (letter) => word.includes(letter) === false
     );
+
     return errorLetters.length;
   };
 
@@ -65,6 +66,7 @@ function App() {
       (letter) =>
         word.toLocaleLowerCase().includes(letter.toLocaleLowerCase()) === false
     );
+
     return errorLetters.map((letter, index) => {
       return (
         <li key={index} className='letter'>
@@ -115,20 +117,7 @@ function App() {
             />
           </form>
         </section>
-        <Dummy {}/>
-        <span className='error-13 eye'></span>
-        <span className='error-12 eye'></span>
-        <span className='error-11 line'></span>
-        <span className='error-10 line'></span>
-        <span className='error-9  line'></span>
-        <span className='error-8  line'></span>
-        <span className='error-7  line'></span>
-        <span className='error-6  head'></span>
-        <span className='error-5  line'></span>
-        <span className='error-4  line'></span>
-        <span className='error-3  line'></span>
-        <span className='error-2  line'></span>
-        <span className='error-1  line'></span>
+        <Dummy numberOfErrors={getNumberOfErrors()} />
       </main>
     </div>
   );
